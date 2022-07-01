adcc8d3d0418deb17c62e03345af0618f7cc3858
Felipe <machadofelipe2016@outlook.com>
2022-07-01 10:05:44 -0300

    c0-stated new code about http and form with react-hook

---
+++ more-http/src/App.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+  
+  const [name, setName]= useState()
+  const [price, setPrice] = useState()
+  
+  
+  
+  return (
+    <div className="App">
+      <h1>Practice and testing form with react-hoock form!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ more-http/src/hoock/useFetch.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+const url =''
+
+export const useFetching = (url) => {
+    const[data, setDate] = useState(null)
+    const[method, setMethod] = useState(null)
+    const[setting, setSetting] = useState(false)
+    const[loading, setLoading] = useState(false)
+
+}
